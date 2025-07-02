031f8def4084a9ea47288e882ae3c8436e662699
ayush <ayushkumarpandey.96@gmail.com>
2025-07-02 22:53:40 +0530

    updated

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
@@ -10,27 +10,72 @@ import snow_icon from '../assets/snow.png'
 import wind_icon from '../assets/wind.png'
 
 const Weather = () => {
+    const inputRef = useRef()
+
+    const [weatherData,setWeatherData]= useState(false);
+
+    const allIcons= {
+        "01d":clear_icon,
+        "01n":clear_icon,
+        "02d":cloud_icon,
+        "02n":cloud_icon,
+        "03d":cloud_icon,
+        "03n":cloud_icon,
+        "04d":drizzle_icon,
+        "04n":drizzle_icon,
+        "09d":rain_icon,
+        "09n":rain_icon,
+        "010d":rain_icon,
+        "010n":rain_icon,
+        "013d":snow_icon,
+        "013n":snow_icon,
+    }
+
+const  search = async (city)=>{
+    try {
+        const url= `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
+
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        const icon = allIcons[data.weather[0].icon]||clear_icon;
+        setWeatherData({
+            humidity:data.main.humidity,
+            windSpeed:data.wind.speed,
+            temperature:Math.floor(data.main.temp),
+            location:data.name,
+            icon: icon,
+        })
+    } catch (error) {
+        
+    }
+
+}
+useEffect (()=>{
+    search("lucknow");
+},[])
+
   return (
     <div className='weather'>
         <div className='search_bar'>
-            <input type="text" placeholder='search' />
-            <img src= {search_icon}   alt="" />
+            <input ref={inputRef} type="text" placeholder='search' />
+            <img src= {search_icon}   alt="" onClick={()=>search(inputRef.current.value)} />
             </div>
-            <img src= {clear_icon} alt="" className='weather_icon' />
-            <p className='temprature'>16 c</p>
-            <p className='location'>London</p>
+            <img src= {weatherData.icon} alt="" className='weather_icon' /> 
+            <p className='temprature'>{weatherData.temperature}°c</p>
+            <p className='location'>{weatherData.location}</p>
             <div className='weather_data'>
                 <div className='col'>
                     <img src= {humidity_icon} alt="" />
                     <div>
-                        <p>91 %</p>
+                        <p>{weatherData.humidity} %</p>
                         <span>Humidity</span>
                     </div>
                 </div>
                   <div className='col'>
                     <img src= {wind_icon} alt="" />
                     <div>
-                        <p> 3.6 km /hr </p>
+                        <p> {weatherData.windSpeed} km/hr </p>
                         <span>wind speed</span>
                     </div>
                 </div>
